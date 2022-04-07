2c8290cb550647cca769a98105d11e08f77dcfde
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2022-04-06 22:19:24 -0400

    fixed some tingz

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -1,10 +1,22 @@
+import os
 from firebase_admin import initialize_app, credentials, firestore, db
 from datetime import datetime, timedelta, date
 import random
 
+
 # Use a service account
-cred = credentials.Certificate(
-    '../ecohub-707c9-firebase-adminsdk-ctcaz-710ec24c6b.json')
+cred = credentials.Certificate({
+    "type": os.environ.get("TYPE"),
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": os.environ.get("AUTH_URI"),
+    "token_uri": os.environ.get("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL")
+})
 initialize_app(
     cred, {'databaseURL': 'https://ecohub-707c9-default-rtdb.firebaseio.com'})
 
